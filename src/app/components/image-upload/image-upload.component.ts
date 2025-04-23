6c5a5ee3386c1632a59900d7967fb64e5f5681de
shyam.i.albert <shyam.i.albert@accenture.com>
2025-04-23 21:30:02 +0530

    style-changes

--- src/app/components/image-upload/image-upload.component.ts
+++ src/app/components/image-upload/image-upload.component.ts
@@ -14,10 +14,12 @@ export class ImageUploadComponent implements OnInit {
   annotations: any[] = [];
   annotatedImage: string | null = null; // Store the annotated image as base64 string
   apiUrl = 'http://localhost:8080/predict/';
+  imageLoader = false;
 
-  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
+    this.imageLoader = false;
   }
 
   predictImage(file: File): Observable<any> {
@@ -30,22 +32,28 @@ export class ImageUploadComponent implements OnInit {
 
   onFileSelected(event: any) {
     const file = event.target.files[0];
-
+    this.imageLoader = false;
     if (file) {
+      const blobUrl = URL.createObjectURL(file);
+      this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(blobUrl);
+      this.imageLoader = true;
+      this.annotatedImage = null;
+      this.predictImage(file).subscribe({
+        next: (response) => {
+          this.imageLoader = false;
+          console.log('Prediction response:', response);
+          let result = JSON.parse(response);
+          this.annotatedImage = result.annotated_image; // Store the base64 string
+          console.log('final response:', this.annotatedImage);
 
-    this.predictImage(file).subscribe({
-      next: (response) => {
-        console.log('Prediction response:', response);
-        let result = JSON.parse(response);
-        this.annotatedImage = result.annotated_image; // Store the base64 string
-        console.log('final response:', this.annotatedImage);
-
-      },
-      error: (error) => {
-        console.error('Error predicting image:', error);
-      }
-    }) // Assuming the response contains the annotated image URL
+        },
+        error: (error) => {
+          this.imageLoader = false;
+          console.error('Error predicting image:', error);
+        }
+      }) // Assuming the response contains the annotated image URL
     } else {
+      this.imageLoader = false;
       console.error('No file selected!');
     }
   }
